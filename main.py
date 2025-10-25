25e446f137d5b82b17e17673ca29a37a7643ade1
FernandoAilon <fernandoailon26@yahoo.com>
2025-10-25 02:17:27 -0400

    intial setup

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow:
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("My PyQt5 Application")
+    

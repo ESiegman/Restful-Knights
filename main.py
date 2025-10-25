68bf910fd3cae6658bb98d0540799e3a6603d72c
Eren Durham <159845643+ErenDurham@users.noreply.github.com>
2025-10-25 14:54:53 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
-class MainWindow:
-  def __init__(self):
-    super().__init__()
-    self.setWindowTitle("My PyQt5 Application")
-    
